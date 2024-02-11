ab57c24ff6d8087d417ed5891e6e16cb8b70103a
a-givertzman <a.givertzman@icloud.com>
2024-02-11 15:02:06 +0300

    initial

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,25 @@
-pub mod test_session;
+///
+/// Basic toolkit for rust project testing purposes
 
+
+/// 
+/// 
+pub mod test_session;
+///
+/// 
 pub mod test_stuff;
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+// pub fn add(left: usize, right: usize) -> usize {
+//     left + right
+// }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
